44bc1035ab3634d5635a0b9f6cd237c25f74e990
ivanfei <ivanfee@outlook.com>
2022-08-15 00:46:48 +0800

    create: adminPage

---
+++ src/views/AdminPage.vue
@@ -0,0 +1,13 @@
+<template>
+  <v-container> </v-container>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'AdminPage'
+})
+</script>
+
+<style scoped></style>
