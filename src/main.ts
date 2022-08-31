63e2ca5e4ea2346208826e2a481f22b04a074d52
Dest1n1 <dest1nis2022@gmail.com>
2022-08-31 09:43:51 +0800

    create: replace vuex with pinia
    Signed-off-by: Dest1n1 <dest1nis2022@gmail.com>

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import { createPinia } from 'pinia'
 
 import { router } from './router'
 import { vuetify } from './plugins/vuetify'
 import './index.scss'
-import { key, store } from '@/store'
 
-createApp(App).use(router).use(vuetify).use(store, key).mount('#app')
+const pinia = createPinia()
+
+createApp(App).use(router).use(vuetify).use(pinia).mount('#app')
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,24 +1,8 @@
-import { createStore, useStore as baseUseStore, Store } from 'vuex'
+import { defineStore } from 'pinia'
+import { reactive } from 'vue'
 import { Division } from '@/types'
-import { InjectionKey } from 'vue'
 
-type DState = {
-  divisions: Division[]
-}
-
-export const store = createStore<DState>({
-  state: {
-    divisions: []
-  },
-  mutations: {
-    setDivisions(state, payload: { divisions: Division[] }) {
-      state.divisions = payload.divisions
-    }
-  }
+export const useStore = defineStore('counter', () => {
+  const divisions = reactive<Division[]>([])
+  return { divisions }
 })
-
-export const key: InjectionKey<Store<DState>> = Symbol()
-
-export const useStore = () => {
-  return baseUseStore(key)
-}
