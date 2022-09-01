fe1bf21a10c558db36e5c65d9ebcef9d2ebe663b
ethanzzl <tehzl1998@gmail.com>
2022-09-01 23:35:05 +0800

    feat: write my own mock

--- src/views/DivisionPage.vue
+++ src/views/DivisionPage.vue
@@ -7,11 +7,6 @@
             <div class="flex grow-0">树洞</div>
             <v-btn>发布树洞</v-btn>
           </div>
-
-
-
-
-
           <v-list-item class="pl-16 py-5 border-b flex-col text-left">
             <div class="w-full flex justify-between my-1">
               <div class="flex items-center space-x-6">
@@ -383,7 +378,9 @@ import TagChip from '@/components/tag/TagChip.vue';
 import SpecialFlagChip from '@/components/tag/SpecialFlagChip.vue';
 import { arrayFactory } from '@/utils/reflect'
 import { camelizeKeys } from '@/utils'
-import { Tag } from '@/types';
+import { Tag, Floor, Hole } from '@/types';
+
+let mode = 1;
 
 const tags = arrayFactory(
   Tag,
@@ -459,6 +456,148 @@ const manyTags = arrayFactory(
 
 const specialTag = '测试用例';
 
+const content = [
+  "我们怀揣热忱的灵魂天然被赋予对超越性的追求，不屑于古旧坐标的约束，钟情于在别处的芬芳。但当这种期望流于对过去观念不假思索的批判，乃至走向虚无与达达主义时，便值得警惕了。与秩序的落差、错位向来不能为越矩的行为张本。而纵然我们已有翔实的蓝图，仍不能自持已在浪潮之巅立下了自己的沉锚。",
+  "“我的生活故事始终内嵌在那些我由之获得自身身份共同体的故事之中。”麦金太尔之言可谓切中了肯綮。人的社会性是不可祓除的，而我们欲上青云也无时无刻不在因风借力。社会与家庭暂且被我们把握为一个薄脊的符号客体，一定程度上是因为我们尚缺乏体验与阅历去支撑自己的认知。而这种偏见的傲慢更远在知性的傲慢之上。",
+  "在孜孜矻矻以求生活意义的道路上，对自己的期望本就是在与家庭与社会对接中塑型的动态过程。而我们的底料便是对不同生活方式、不同角色的觉感与体认。生活在树上的柯希莫为强盗送书，兴修水利，又维系自己的爱情。他的生活观念是厚实的，也是实践的。倘若我们在对过往借韦伯之言“祓魅”后，又对不断膨胀的自我进行“赋魅”，那么在丢失外界预期的同时，未尝也不是丢了自我。",
+  "毫无疑问，从家庭与社会角度一觇的自我有偏狭过时的成分。但我们所应摒弃的不是对此的批判，而是其批判的廉价，其对批判投诚中的反智倾向。在尼采的观念中，如果在成为狮子与孩子之前，略去了像骆驼一样背负前人遗产的过程，那其“永远重复”洵不能成立。何况当矿工诗人陈年喜顺从编辑的意愿，选择写迎合读者的都市小说，将他十六年的地底生涯降格为桥段素材时，我们没资格斥之以媚俗。",
+  "蓝图上的落差终归只是理念上的区分，在实践场域的分野也未必明晰。譬如当我们追寻心之所向时，在途中涉足权力的玉墀，这究竟是伴随着期望的泯灭还是期望的达成？在我们塑造生活的同时，生活也在浇铸我们。既不可否认原生的家庭性与社会性，又承认自己的图景有轻狂的失真，不妨让体验走在言语之前。用不被禁锢的头脑去体味切斯瓦夫·米沃什的大海与风帆，并效维特根斯坦之言，对无法言说之事保持沉默。",
+  "用在树上的生活方式体现个体的超越性，保持婞直却又不拘泥于所谓“遗世独立”的单向度形象。这便是卡尔维诺为我们提供的理想期望范式。生活在树上——始终热爱大地——升上天空。"
+];
+const objectFloors = camelizeKeys([
+    {
+      content: content[0],
+      holeId: 114514,
+      specialTag: specialTag,
+      anonyname: 'Rick Ashley',
+      deleted: false,
+      id: 1919810,
+      storey: 1,
+      fold: '',
+      like: 0,
+      time_created: '2022-07-27',
+      time_updated: '2022-07-27'
+    },
+    {
+      content: content[1],
+      holeId: 114515,
+      specialTag: specialTag,
+      anonyname: 'Rick Ashley',
+      deleted: false,
+      id: 1919810,
+      storey: 1,
+      fold: '',
+      like: 0,
+      time_created: '2022-07-27',
+      time_updated: '2022-07-27'
+    },
+    {
+      content: content[2],
+      holeId: 114516,
+      specialTag: specialTag,
+      anonyname: 'Rick Ashley',
+      deleted: false,
+      id: 1919810,
+      storey: 1,
+      fold: '',
+      like: 0,
+      time_created: '2022-07-27',
+      time_updated: '2022-07-27'
+    },
+    {
+      content: content[3],
+      holeId: 114517,
+      specialTag: specialTag,
+      anonyname: 'Rick Ashley',
+      deleted: false,
+      id: 1919810,
+      storey: 1,
+      fold: '',
+      like: 0,
+      time_created: '2022-07-27',
+      time_updated: '2022-07-27'
+    },
+    {
+      content: content[4],
+      holeId: 114518,
+      specialTag: specialTag,
+      anonyname: 'Rick Ashley',
+      deleted: false,
+      id: 1919810,
+      storey: 1,
+      fold: '',
+      like: 0,
+      time_created: '2022-07-27',
+      time_updated: '2022-07-27'
+    },
+    {
+      content: content[5],
+      holeId: 114514,
+      specialTag: specialTag,
+      anonyname: 'Linus',
+      deleted: false,
+      id: 1919810,
+      storey: 2,
+      fold: '',
+      like: 0,
+      time_created: '2022-07-27',
+      time_updated: '2022-07-27'
+    },
+  ])
+const floors = arrayFactory(
+  Floor,
+  objectFloors
+)
+console.log(tags);
+const holeArray = camelizeKeys([
+    {
+      division_id: 1,
+      id: 114514,
+      floors: objectFloors,
+      first_floor: floors[0],
+      last_floor: floors[5],
+      reply: 19,
+      tags: tags,
+      time_created: '2022-07-27',
+      time_updated: '2022-07-27'
+    },
+    {
+      division_id: 1,
+      id: 114515,
+      floors: objectFloors,
+      first_floor: floors[1],
+      last_floor: floors[5],
+      reply: 1919,
+      tags: tags,
+      time_created: '2022-07-27',
+      time_updated: '2022-07-27'
+    },
+    {
+      division_id: 1,
+      id: 114516,
+      floors: objectFloors,
+      first_floor: floors[2],
+      last_floor: floors[5],
+      reply: 1,
+      tags: tags,
+      time_created: '2022-07-27',
+      time_updated: '2022-07-27'
+    },
+    {
+      division_id: 1,
+      id: 114517,
+      floors: objectFloors,
+      first_floor: floors[3],
+      last_floor: floors[5],
+      reply: 190,
+      tags: tags,
+      time_created: '2022-07-27',
+      time_updated: '2022-07-27'
+    }
+  ]);
+console.log("floors: ", floors)
+debugger
+const holes = arrayFactory(Hole, holeArray);
 </script>
 
 <style
