68544000ab1cfe167eaaa89021fbe0ec369412a6
Dest1n1s <dest1nis2022@gmail.com>
2023-07-22 15:54:07 +0800

    feat: change router to HTML5 mode

--- src/router.ts
+++ src/router.ts
@@ -1,4 +1,4 @@
-import { createRouter, createWebHashHistory, RouteRecordRaw } from 'vue-router'
+import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
 import DivisionPage from '@/views/DivisionPage.vue'
 import HolePage from '@/views/HolePage.vue'
 import AdminPage from '@/views/AdminPage.vue'
@@ -143,6 +143,6 @@ export const routes: RouteRecordRaw[] = [
 ]
 
 export const router = createRouter({
-  history: createWebHashHistory(),
+  history: createWebHistory(),
   routes
 })
